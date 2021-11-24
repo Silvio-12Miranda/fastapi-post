f0b8aeb4f7e4b4cf53851b76239cfc5b9fd8bfc5
Silvio Miranda <silviom727@gmail.com>
2021-11-24 02:25:53 -0600

    se agrega la funcionalidad de la ruta post de guardar los post generados por el usuario, al igual se implementa la creacion del modelo creador de objetos

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from uuid import uuid4 as uuid
+from datetime import datetime
+from typing import Text, Optional
+from pydantic import BaseModel
+
 
 # FastAPI
 from fastapi import FastAPI
@@ -5,6 +10,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 posts = []
+
+
+# Post MODEL
+class Post(BaseModel):
+    id : Optional[str]
+    title : str
+    author : str
+    content : Text
+    created_at : datetime = datetime.now()
+    published_at : Optional[datetime]
+    published : bool = False
+
 
 @app.get('/')
 def reas_rooot():
@@ -14,3 +31,12 @@ def reas_rooot():
 @app.get('/posts')
 def get_posts():
     return posts
+
+
+@app.post('/posts')
+def save_posts(post:Post):
+    #print(uuid())
+    post.id = str(uuid())
+    posts.append(post.dict())
+    #print(post.dict())
+    return posts[-1]
